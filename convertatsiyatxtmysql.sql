80163ed8c37014389a5ad9a0b1e134b6c440e40f
Muhammadqodir <136580277+BAJARUVCHI@users.noreply.github.com>
2024-09-20 17:51:46 +0500

    --v

---
+++ convertatsiyatxtmysql.sql
@@ -0,0 +1,6 @@
+﻿LOAD DATA INFILE 'D:/Github/Loyihalar/WEBQ/Savollar.txt'
+INTO TABLE savol
+FIELDS TERMINATED BY ';' 
+LINES TERMINATED BY '\n'
+(savol, javob);
+mysql --local-infile -u username -p
